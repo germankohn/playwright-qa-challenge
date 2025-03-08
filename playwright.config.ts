d1e3e329d03a2fd92d4cd9187f90be071184ef5f
germankohnredbee <germankohn@gmail.com>
2025-03-08 13:25:40 -0300

    mejoras en el workflow de actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,74 +21,25 @@ if (!BASE_URL) {
 
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['json', { outputFile: 'test-results.json' }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 20000, // Tiempo máximo para cada test
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     headless: true,
-    trace: 'on', // Habilitar trazas para depuración
-    video: 'on', // Capturar videos de las ejecuciones de los tests
+    trace: 'on',
+    video: 'on',
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'Google Chrome',
       use: {
         ...devices['Desktop Chrome'],
-        channel: 'chrome' ,
-        baseURL: BASE_URL
+        channel: 'chrome',
+        baseURL: BASE_URL,
       },
-     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
