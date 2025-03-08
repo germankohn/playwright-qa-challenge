9c266dbb39559d93a4a831727dbd78df4d8b1d98
germankohnredbee <germankohn@gmail.com>
2025-03-08 13:19:19 -0300

    mejoras en el workflow de actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['json', { outputFile: 'test-results.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 20000, // Tiempo máximo para cada test
   use: {
@@ -38,8 +38,9 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
     headless: true,
+    trace: 'on', // Habilitar trazas para depuración
+    video: 'on', // Capturar videos de las ejecuciones de los tests
   },
 
   /* Configure projects for major browsers */
